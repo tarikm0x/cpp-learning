3666feaa38abc74fc0b9a844edb773ed72fc8f5b
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-12 23:59:00 +0100

    02-operatori-izrazi: usporedba.cpp

--- 02-operatori-izrazi/usporedba.cpp
+++ 02-operatori-izrazi/usporedba.cpp
@@ -2,12 +2,35 @@
 using namespace std;
 
 int main() {
-
 	int a, b;
 	cout << "Unesi dva broja: " << endl;
 	cin >> a >> b;
 
-
+	if (a == b) {
+		cout << "Brojevi su jednaki!" << endl;
+	}
+	if (a != b) {
+		cout << "Brojevi nisu jednaki!" << endl;
+	}
+	if (a > b) {
+		cout << a << " je veci od " << b << endl;
+	}
+	if (a < b) {
+		cout << a << " je manji od " << b << endl;
+	}
+	if (a >= b) {
+		cout << a << " je veci ili jednak od " << b << endl;
+	}
+	if (a <= b) {
+		cout << a << " je manji ili jednak od " << b << endl;
+	}
+	cout << endl;
+	cout << a << " == " << b << " : " << (a == b) << endl;
+	cout << a << " != " << b << " : " << (a != b) << endl;
+	cout << a << " >  " << b << " : " << (a > b) << endl;
+	cout << a << " <  " << b << " : " << (a < b) << endl;
+	cout << a << " >= " << b << " : " << (a >= b) << endl;
+	cout << a << " <= " << b << " : " << (a <= b) << endl;
 
 	return 0;
 }
