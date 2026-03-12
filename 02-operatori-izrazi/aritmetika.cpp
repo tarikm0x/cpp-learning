b54e77aeb8ad2d25a0fd4ae1e528e813b38ef5fc
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-12 23:22:46 +0100

    02-operatori-izrazi: aritmetika.cpp

---
+++ 02-operatori-izrazi/aritmetika.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Unesi 2 broja: " << endl;
+	int a, b;
+	cin >> a >> b;
+	cout << "Zbir: " << a + b << endl;
+	cout << "Razlika: " << a - b << endl;
+	cout << "Proizvod: " << a * b << endl;
+	cout << "Kolicnik: " << a / b << endl;
+	cout << "Ostatak: " << a % b << endl;
+
+
+	return 0;
+}
--- cpp-learning/cpp-learning.cpp
+++ cpp-learning/cpp-learning.cpp
@@ -3,18 +3,6 @@ using namespace std;
 
 int main() {
 
-	int _int= 5;
-	double _double = 3.14159265359;
-	float _float = 3.14159265359;
-	char _char = 'a';
-	bool _bool = true;
-
-	cout << "int -> " << _int <<" (size: "<<sizeof(_int)<<" bytes)"<< endl;
-	cout << "double -> " << _double << " (size: " << sizeof(_double) << " bytes)" << endl;
-	cout << "float -> " << _float << " (size: " << sizeof(_float) << " bytes)" << endl;
-	cout << "char -> " << _char << " (size: " << sizeof(_char) << " bytes)" << endl;
-	cout << "bool=true -> " << _bool << " (size: " << sizeof(_bool) << " bytes)" << endl;
-
 
 
 
