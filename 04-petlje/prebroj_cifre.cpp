a480309eb69723305b95eacd6436536c783828ab
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-14 04:48:08 +0100

    04-petlje: prebroj_cifre.cpp

--- 04-petlje/prebroj_cifre.cpp
+++ 04-petlje/prebroj_cifre.cpp
@@ -6,6 +6,12 @@ int main() {
 	cout << "Unesi broj: ";
 	cin >> broj;
 
+	int i = 0;
+	while (broj != 0) {
+		broj = broj / 10;
+		i++;
+	}
+	cout << "Broj ima: " << i << " cifre!";
 	return 0;
 
 }
