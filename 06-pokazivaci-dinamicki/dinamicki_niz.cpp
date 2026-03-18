546c070ff688ec17be0ca7e18b7abdce64161e9a
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-18 01:54:14 +0100

    06-pokazivaci-dinamicki: dinamicki_niz.cpp

--- 06-pokazivaci-dinamicki/dinamicki_niz.cpp
+++ 06-pokazivaci-dinamicki/dinamicki_niz.cpp
@@ -10,10 +10,10 @@ int main() {
 	int* p = niz;
 	for (int i = 0; i < n; i++) {
 		cout << "Unesi " << i + 1 << " clan niza:";
-		cin >> *(p+i);
+		cin >> *(p + i);
 	}
 	for (int i = 0; i < n; i++) {
-		cout << *(niz+i)<<" ";
+		cout << *(niz + i) << " ";
 	}
 	delete[] niz;
 
