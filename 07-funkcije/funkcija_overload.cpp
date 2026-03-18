2615e723b390faa194400ea3031a0a95218d53cb
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-19 00:22:40 +0100

    07-funkcije: funkcija_overload.cpp

--- 07-funkcije/funkcija_overload.cpp
+++ 07-funkcije/funkcija_overload.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int zbir(int a, int b) {
 
+	return a + b;
+}
+double zbir(double a, double b) {
+
+	return a + b;
+}
+
+int main() {
+	int a = 5;
+	int b = 6;
+
+	double x = 3.5;
+	double y = 2.1;
+	cout << zbir(a, b) << endl;
+	cout << zbir(x, y) << endl;
 
 	return 0;
 
 }
+// C++ automatski odabere pravu funkciju na osnovu tipa argumenta 
+// to je overloading
