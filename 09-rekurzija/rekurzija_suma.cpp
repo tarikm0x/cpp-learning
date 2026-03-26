524bcf1e1cd545eabdcf95dd0adb56c62cbf9618
Tarik <35664234+wh0ami0x@users.noreply.github.com>
2026-03-26 04:04:51 +0100

    09-rekurzija: rekurzija_suma.cpp

--- 09-rekurzija/rekurzija_suma.cpp
+++ 09-rekurzija/rekurzija_suma.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
+int suma(int broj) {
+
+	if (broj <= 0) return 0;
+	return broj + suma(broj - 1);
+}
 
 int main() {
 
 	cout << "Unesi broj: ";
 	int a;
 	cin >> a;
-	
-	
+	cout << suma(a);
+
 	return 0;
 }
